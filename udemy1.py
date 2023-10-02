b099bcc03b349625dab2ace80720f97a2c7aafb3
Gabriel Santos-Martins <gabrielsantos-martins@Gabriels-MacBook-Pro.local>
2023-10-02 19:45:04 -0400

    add calculation file

---
+++ udemy1.py
@@ -0,0 +1,11 @@
+# BMI = weight in kgs / (height in m)ˆ2 
+
+weight = float(input("Enter weight in kilograms: "))
+height = float(input("Enter height in meters: "))
+
+BMI = weight/(height**2)
+
+print(f"BMI is {BMI}")
+
+
+
